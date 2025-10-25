354a63b1a8c6228e21ad4fe6ab76e64c71f09e06
Amira <bcuba@unsa.edu.pe>
2025-10-24 22:48:09 -0500

    Subiendo 9no ejercicio

--- script7.js
+++ script7.js
@@ -1,22 +1,20 @@
 let continuar = true;
 while (continuar){    
-    console.log("MENÚ DE OPCIONES");
-    console.log("1. Calcular área del círculo \n2. Calcular área del rectángulo \n3. Salir");
-    let opcion = parseInt(prompt("Ingrese un número:"));
-    switch (opcion) {
-        case 1:
-            let radio = parseFloat(prompt("Ingresa el radio:"));
-            let areaCirculo = ((radio**2)*(3.14)).toFixed(2);
-            console.log(`El área del círculo es ${areaCirculo}`);
-            break;
-        case 2:
-            let ancho = parseFloat(prompt("Ingresa el ancho:"));
-            let alto = parseFloat(prompt("Ingresa el alto:"));
-            let areaRectangulo = (alto*ancho).toFixed(2);
-            console.log(`El área del rectángulo es es ${areaRectangulo}`);
-            break;
-        case 3:
-            continuar = false;
-            break; 
+    let rango = parseInt(prompt("Ingrese el rango:"));
+    let suma = 0;
+    for(let i=1; i<=rango; i++){
+        if (i % 5 == 0){
+            continue;
+        }
+        suma += i;
+    }
+    console.log(`La suma de los números hasta el ${rango}, sin los múltiplos de 5 es ${suma}`);
+    console.log("---------------------------------------");
+    continuar = prompt("¿Quiero seguir? (S/N)").toUpperCase();
+    if (continuar == "S"){
+        continuar = true;
+    }
+    else{
+        continuar = false;
     }
 }
